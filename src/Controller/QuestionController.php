87710699dde40be72c3ccdad923396cc2e8950cd
MScheltjens <mathias.scheltjens@gmail.com>
2022-03-24 15:33:43 +0100

    my first twig template

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -2,10 +2,11 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class QuestionController
+class QuestionController extends AbstractController
 {
     /**
      * @Route("/")
@@ -19,6 +20,8 @@ class QuestionController
      */
     public function show($slug)
     {
-        return new Response("question $slug!");
+        return $this->render('question/show/html.twig',[
+            'question' => ucwords(str_replace('-', '', $slug))
+        ]);
     }
 }
